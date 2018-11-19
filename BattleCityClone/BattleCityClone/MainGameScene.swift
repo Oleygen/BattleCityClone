be96ddc8c37f7f3a01c993bd64d65a7fb9b60c0c
oleygen ua <oleygenua@oleygens-iMac.local>
2018-11-19 02:37:50 +0200

    Added player-1 stub node

--- BattleCityClone/BattleCityClone/MainGameScene.swift
+++ BattleCityClone/BattleCityClone/MainGameScene.swift
@@ -14,6 +14,7 @@ class MainGameScene: SKScene {
     private enum NodeName: String
     {
         case tileMap = "tileMap"
+        case player1 = "player-1"
     }
     
     private var minimumSizeDimension : CGFloat
@@ -23,6 +24,8 @@ class MainGameScene: SKScene {
     
     
     var tileMap : SKTileMapNode?
+    var player1Tank : SKSpriteNode?
+    
     override func willMove(from view: SKView) {
         super.willMove(from: view)
         print(#function)
@@ -34,7 +37,7 @@ class MainGameScene: SKScene {
         self.size = view.frame.size
         
         tileMap = self.childNode(withName: NodeName.tileMap.rawValue) as? SKTileMapNode
-        
+        player1Tank = self.childNode(withName: NodeName.player1.rawValue) as? SKSpriteNode
         let size = CGSize(width: minimumSizeDimension, height: minimumSizeDimension)
         
         // let tileGroup = SKTileGroup(rules: <#T##[SKTileGroupRule]#>)
